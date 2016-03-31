fed8cc934640c2f2530e3f7fc841dcb5c5ffd05e
Tuomas Jomppanen <tuomas@jomppanen.com>
2016-04-01 00:14:32 +0300

    few db records for just testing purposes

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Word.delete_all
+Translation.delete_all
+beana = Word.create!( id: 0, body: "beana", keyword: "beana", language_id: Language.find_by_code("northern_sami"))
+dog = Word.create!( id: 1, body: "dog", keyword: "dog", language_id: Language.find_by_code("english"))
+koira = Word.create!( id: 2, body: "koira", keyword: "koira", language_id: Language.find_by_code("finnish"))
+Word.create!( id: 3, body: "begin", keyword: "begin", language_id: Language.find_by_code("english"))
+Word.create!( id: 4, body: "beduiini", keyword: "beduiini", language_id: Language.find_by_code("finnish"))
+Translation.create!(id: 0, original: beana, translated: dog)
+Translation.create!(id: 1, original: dog, translated: beana)
+Translation.create!(id: 2, original: beana, translated: koira)
+Translation.create!(id: 3, original: koira, translated: beana)
